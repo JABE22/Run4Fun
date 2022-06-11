c53a9bf79b4a62f270955c4b7c6c4f30fced93eb
Jarno Matarmaa <jarno.matarmaa@tuni.fi>
2022-06-11 17:08:40 +0500

    Python csv data parser fix

--- run4fun/events/static/python/csv-transformer.py
+++ run4fun/events/static/python/csv-transformer.py
@@ -1,7 +1,3 @@
-import pandas as pd
-from ccodes import CODES, aplha2to3
-import os
-
 '''
 NOTE: 
 * Run this file from the path it is located in order to successful read and write from the static file paths (hard coded)
@@ -15,29 +11,38 @@ NOTE:
   >> python csv-transformer.py
 '''
 
+import pandas as pd
+from ccodes import CODES, aplha2to3
+import os
 
 # CSV transformer for data from Königsschlösser Marathon, Füssen
 def füssen():
     #print(os.path)
     data = pd.read_csv('data/raceresults/füs-halfmar-raw.csv', delimiter='\t')
+    
     # Removes decimals from place indicator columns
     data['Platz'] = data['Platz'].astype('int32')
     data['M'] = data['M'].fillna(0).astype('int32').astype('str').replace('0','')
     data['W'] = data['W'].fillna(0).astype('int32').astype('str').replace('0','')
+
     # Combines first and last names
     data['Vorname'] = data['Vorname'] + " " + data['Nachname']
+
     # Renames and drops unused columns from the data
     data = data.rename(columns={'Vorname':'Name','Nation':'CountryCode','AK':'Category','Netto':'Time'})
     data = data.drop(columns=['M','W','Nachname','AK-Platz'])
     print(data.columns)
+
     # Country code modifications
     print(data['CountryCode'].unique())
     data['CountryCode'] = data['CountryCode'].fillna('ukw').apply(lambda x: aplha2to3.get(x))
     print(data['CountryCode'])
+
     # Inserts Nation column to achieve compatibility for some specific application
     nations = data['CountryCode'].apply(lambda x: CODES.get(x))
     data.insert(3,'Nation',nations)
     print(data['Nation'])
+
     # Flip column order of Club and category
     data = data[['Platz','Nr.','Name','Nation','CountryCode','Verein','Category','Time','Brutto']]
     print(data.columns)
